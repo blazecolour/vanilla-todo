6a0cd37e83963c1641826b66afefee63bd8f57e1
blazecolour <936x936@gmail.com>
2018-09-15 20:16:51 +0300

    add sass-loader to  webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/js/index.js'],
+  entry: ['./src/js/index.js', './src/scss/style.scss'],
   output: {
     filename: './js/bundle.js'
   },
@@ -17,8 +18,34 @@ module.exports = {
             presets: 'env'
           }
         }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                url: false
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }
     ]
   },
-  plugins: []
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true
+    })
+  ]
 };
