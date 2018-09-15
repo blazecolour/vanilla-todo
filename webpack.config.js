ed66ccac8ff0c2c9b7b3361b58892f7409d0027a
blazecolour <936x936@gmail.com>
2018-09-15 21:36:47 +0300

    update babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,12 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const fs = require('fs')
-
-function generateHtmlPlugins(templateDir) {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
-    const parts = item.split('.');
-    const name = parts[0];
-    const extension = parts[1];
-    return new HtmlWebpackPlugin({
-      filename: `${name}.html`,
-      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-      inject: false,
-    })
-  })
-}
-
-const htmlPlugins = generateHtmlPlugins('./src/html/views')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
 
 module.exports = {
   entry: ['./src/js/index.js', './src/scss/style.scss'],
   output: {
+    path: path.resolve(__dirname, './dist'),
     filename: './js/bundle.js'
   },
   devtool: 'source-map',
@@ -63,6 +49,7 @@ module.exports = {
     new ExtractTextPlugin({
       filename: './css/style.bundle.css',
       allChunks: true
-    })
-  ].concat(htmlPlugins)
+    }),
+    new HtmlWebpackPlugin()
+  ]
 };
